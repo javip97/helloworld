b0bb72c9e14fa9b7a743c76c9f6135afb6781ad9
javier.perez3 <javier.perez@datavision.io>
2020-11-27 14:03:03 +0100

    fd

--- main.go
+++ main.go
@@ -9,15 +9,30 @@ func main() {
 
 	fd, err := syscall.Open("something.txt", syscall.O_RDWR, 755)
 	if err != nil {
+		log.Println("there has been an error")
 		log.Print(err)
+		return
 	}
+
+	log.Println("no error keep executing")
 	log.Printf("File Descriptor: %v\n", fd)
 
+	bytesWritten, err := syscall.Write(fd, []byte("hello world from fd"))
+	if err != nil {
+		log.Println("error with writting fd")
+		log.Println(err)
+		return
+	}
+
+	fd, err = syscall.Open("something.txt", syscall.O_RDWR, 755)
+	log.Println("bytes written", bytesWritten)
 	//Read from file using File Descriptor
-	data := make([]byte, 12)
+	data := make([]byte, 50)
 	n, err := syscall.Read(fd, data)
 	if err != nil {
+		log.Println("error syscall read")
 		log.Print(err)
+		return
 	}
 	log.Printf("Read %d bytes", n)
 	log.Printf("Content: %s", data)
