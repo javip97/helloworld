3568780ad410a339793b918112c516e3d9b768b2
javier.perez3 <javier.perez@datavision.io>
2020-11-27 15:18:49 +0100

    last chance

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 
-	path := flag.String("path", "/home/guest/go/helloworld-main/testfile.txt", "usage")
+	path := flag.String("path", "testfile.txt", "set the path variable to exampleOutput.txt")
 	flag.Parse()
 
 	fd, err := syscall.Open(*path, syscall.O_CREAT|syscall.O_RDWR|syscall.O_TRUNC, 755)
@@ -21,15 +21,15 @@ func main() {
 	log.Println(fd)
 	log.Println(*path)
 
-	fd, err = syscall.Open(*path, syscall.O_RDWR, 755)
-	if err != nil {
-		log.Println("there has been an error")
-		log.Print(err)
-		return
-	}
+	// fd, err = syscall.Open(*path, syscall.O_RDWR, 755)
+	// if err != nil {
+	// 	log.Println("there has been an error")
+	// 	log.Print(err)
+	// 	return
+	// }
 
-	log.Println("no error keep executing")
-	log.Printf("File Descriptor: %v\n", fd)
+	// log.Println("no error keep executing")
+	// log.Printf("File Descriptor: %v\n", fd)
 
 	bytesWritten, err := syscall.Write(fd, []byte("hello world from fd"))
 	if err != nil {
@@ -37,17 +37,18 @@ func main() {
 		log.Println(err)
 		return
 	}
+	log.Println(bytesWritten)
 
-	fd, err = syscall.Open(*path, syscall.O_RDWR, 755)
-	log.Println("bytes written", bytesWritten)
-	//Read from file using File Descriptor
-	data := make([]byte, 50)
-	n, err := syscall.Read(fd, data)
-	if err != nil {
-		log.Println("error syscall read")
-		log.Print(err)
-		return
-	}
-	log.Printf("Read %d bytes", n)
-	log.Printf("Content: %s", data)
+	// fd, err = syscall.Open(*path, syscall.O_RDWR, 755)
+	// log.Println("bytes written", bytesWritten)
+	// //Read from file using File Descriptor
+	// data := make([]byte, 50)
+	// n, err := syscall.Read(fd, data)
+	// if err != nil {
+	// 	log.Println("error syscall read")
+	// 	log.Print(err)
+	// 	return
+	// }
+	// log.Printf("Read %d bytes", n)
+	// log.Printf("Content: %s", data)
 }
